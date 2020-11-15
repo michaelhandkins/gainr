aeb8aecfa543e22551bb8d998f24a7fbdcf641d2
Michael Handkins <michaelhandkins@gmail.com>
2020-11-15 14:27:16 -0600

    Subtract user entered calories consumed from goal in order to properly reflect calories remaining on UI. Save user entered calories consumed to user defaults. Add button that resets calories remaining

--- gainr/Controller/CalorieViewController.swift
+++ gainr/Controller/CalorieViewController.swift
@@ -15,11 +15,36 @@ class CalorieViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var caloriesConsumed: UITextField!
     
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        allCalories = [0]
+        defaults.setValue(allCalories, forKey: "caloriesEaten")
+        caloriesRemaining.text = String(userGoal)
+    }
+    
+    
     let defaults = UserDefaults.standard
+    var allCalories: [Int] = [0]
+    var totalConsumed: Int {
+        allCalories.reduce(0, +)
+    }
+    var userGoal: Int {
+        if let userEnteredGoal = defaults.string(forKey: "userGoal") {
+            return Int(userEnteredGoal)!
+        } else {
+            return 2000
+        }
+    }
+    
+    var caloriesRemainingValue: Int {
+        userGoal - totalConsumed
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.addDoneButtonOnKeyboard()
         goalField.delegate = self
+        caloriesConsumed.delegate = self
         
         if let safeGoal = defaults.string(forKey: "userGoal") {
             goalField.text = safeGoal
@@ -27,17 +52,48 @@ class CalorieViewController: UIViewController, UITextFieldDelegate {
             goalField.placeholder = "2000"
         }
         
-        if goalField.text != "" {
-            caloriesRemaining.text = goalField.text
+        if let safeAllCalories = defaults.array(forKey: "caloriesEaten") {
+            allCalories = safeAllCalories as! [Int]
         }
+        caloriesRemaining.text = String(caloriesRemainingValue)
+        
+        
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
         if textField == goalField {
-            caloriesRemaining.text = textField.text
             defaults.setValue(textField.text, forKey: "userGoal")
+            caloriesRemaining.text = String(caloriesRemainingValue)
+        } else if textField == caloriesConsumed {
+            if textField.text != "" {
+                allCalories.append(Int(textField.text!)!)
+                defaults.setValue(allCalories, forKey: "caloriesEaten")
+                caloriesRemaining.text = String(caloriesRemainingValue)
+                textField.text = ""
+            }
         }
     }
+    
+    func addDoneButtonOnKeyboard(){
+            let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+            doneToolbar.barStyle = .default
+
+            let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+            let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+
+            let items = [flexSpace, done]
+            doneToolbar.items = items
+            doneToolbar.sizeToFit()
+
+            goalField.inputAccessoryView = doneToolbar
+            caloriesConsumed.inputAccessoryView = doneToolbar
+        }
+
+        @objc func doneButtonAction(){
+            goalField.resignFirstResponder()
+            caloriesConsumed.resignFirstResponder()
+        }
+
 
 
 }
