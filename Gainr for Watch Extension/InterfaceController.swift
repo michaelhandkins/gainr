0fa981d95156fdd98fd201a1c8e390b7a223abf3
Michael Handkins <michaelhandkins@gmail.com>
2020-11-15 15:46:22 -0600

    Set up Apple Watch companion UI

---
+++ Gainr for Watch Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  Gainr for Watch Extension
+//
+//  Created by Michael Handkins on 11/15/20.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
